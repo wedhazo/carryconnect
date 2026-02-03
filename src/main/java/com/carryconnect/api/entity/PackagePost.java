297f5d9bca1da78e8e2d90232407381de2795be5
wedhazo <kwedhazo@gmail.com>
2026-02-03 17:11:47 -0500

    Upgrade Java version from 17 to 21 LTS

--- src/main/java/com/carryconnect/api/entity/PackagePost.java
+++ src/main/java/com/carryconnect/api/entity/PackagePost.java
@@ -1,7 +1,9 @@
 package com.carryconnect.api.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
